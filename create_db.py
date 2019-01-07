20d73cd9a2161f501c298f2914e98481c879b328
YarinNahum <yarinna@post.bgu.ac.il>
2019-01-07 19:51:11 +0200

    erkjgDSKJLbcdsjkdsavbkljdsbvkjdsf,b dfkjlvejlkvebgkjaevg,jsdbds,jbgaewlkj bewgjl

---
+++ create_db.py
@@ -0,0 +1,75 @@
+import sys
+import os
+import sqlite3
+
+isDataBaseExists = os.path.isfile('classes.db')
+dbcon = sqlite3.connect('classes.db')
+
+with dbcon:
+    cursor = dbcon.cursor()
+    if not isDataBaseExists:
+        cursor.execute("""CREATE TABLE classrooms(
+        id INTEGER PRIMARY KEY,
+        location TEXT NOT NULL,
+        current_course_id INTEGER NOT NULL,
+        current_course_time_left INTEGER NOT NULL)
+        """)
+
+        cursor.execute("""CREATE TABLE courses(
+        id INTEGER PRIMARY KEY,
+        course_name TEXT NOT NULL,
+        student TEXT NUT NULL,
+        number_of_students INTEGER NOT NULL,
+        class_id INTEGER REFERENCES classrooms(id),
+        course_length INTEGER NOT NULL)
+        """)
+
+        cursor.execute("""CREATE TABLE students(
+        grade TEXT PRIMARY KEY,
+        count INTEGER NOT NULL)
+        """)
+
+
+def addConfigFile(args):
+    configFilePath = args[1]
+    if not isDataBaseExists:
+        with open(configFilePath) as configFile:
+            for line in configFile:
+                lineList = line.split(',')
+                if lineList[0] == 'C':
+                    addCourse(lineList)
+                elif lineList[0] == 'S':
+                    addStudent(lineList)
+                elif lineList[0] == 'R':
+                    addRoom(lineList)
+            configFile.close()
+
+
+
+def addCourse(lineList):
+    cursor.execute("INSERT INTO courses VALUES(?,?,?,?,?,?)", (lineList[1], lineList[2], lineList[3], lineList[4], lineList[5], lineList[6]))
+
+
+def addStudent(lineList):
+    cursor.execute("INSERT INTO students VALUES(?,?)", (lineList[1], lineList[2]))
+
+
+def addRoom(lineList):
+    cursor.execute("INSERT INTO classrooms VALUES(?,?,?,?)", (lineList[1], lineList[2], 0, 0))
+
+
+if __name__ == '__main__':
+    addConfigFile(sys.argv)
+
+
+cursor.execute("SELECT * FROM courses")
+for course in cursor.fetchall():
+    print(course)
+
+cursor.execute("SELECT * FROM classrooms")
+for classroom in cursor.fetchall():
+    print(classroom)
+
+cursor.execute("SELECT * FROM students")
+for student in cursor.fetchall():
+    print(student)
